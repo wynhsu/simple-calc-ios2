ce0283bc150567d8173a2ee4937a518ca8205dbf
Wynston <wynhsu@uw.edu>
2018-10-17 00:59:04 -0700

    finish basic op

--- simple-calc/simple-calc/ViewController.swift
+++ simple-calc/simple-calc/ViewController.swift
@@ -26,12 +26,57 @@ class ViewController: UIViewController {
         output.text! = val
     }
     
-    @IBAction func numberPressed(_ sender: UIButton) {
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
         let val = sender.currentTitle!
         let newStr = outputString.text! + val
-//        result += Int(val)!
         outputString.text! = newStr
     }
     
+    @IBAction func clearPressed(_ sender: UIButton) {
+        outputString.text! = ""
+        output.text! = "0"
+    }
+    
+    @IBAction func compute(_ sender: UIButton) {
+        let eq = outputString.text!
+        var first = ""
+        var second = ""
+        var operand = ""
+        for i in 0..<eq.count {
+            let index = String(eq[eq.index(eq.startIndex, offsetBy: i)])
+            if (index == "÷") || (index == "x") || (index == "-") || (index == "+") || (index == "%") {
+                operand = index
+            } else if operand != "" {
+                second += index
+            } else {
+                first += index
+            }
+        }
+        print(first)
+        print(operand)
+        print(second)
+        var result = 0
+        let arg1 = Int(first)!
+        let arg2 = Int(second)!
+        print(arg1)
+        print(arg2)
+        switch operand {
+        case "÷":
+            result = arg1 / arg2
+        case "x":
+            result = arg1 * arg2
+        case "-":
+            result = arg1 - arg2
+        case "+":
+            result = arg1 + arg2
+        case "%":
+            result = arg1 % arg2
+        default:
+            result = 0
+        }
+        output.text! = String(result)
+        outputString.text! = ""
+    }
 }
 
