e0d7a16173075a3b0a06aaf9526ff9c71b33c3bf
Wynston <wynhsu@uw.edu>
2018-10-21 03:52:19 -0700

    nav controller; need finish history view

---
+++ simple-calc/simple-calc/HistoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HistoryViewController.swift
+//  simple-calc
+//
+//  Created by Wynston Hsu on 10/20/18.
+//  Copyright © 2018 Wynston Hsu. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
